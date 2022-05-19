437c2b7cd7397e48f4eb6dbef23e851e761a3e8f
subash kc <subaazkc@gmail.com>
2022-05-19 21:04:10 +0545

    get weather location feature added

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/loction_provider.dart';
@@ -15,6 +16,42 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _locationNameController = TextEditingController();
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+  }
+
+  @override
+  void initState() {
+    _determinePosition().then((position) =>
+        Provider.of<LocationProvider>(context, listen: false)
+            .getWeatherByPosition(position.latitude, position.longitude));
+    super.initState();
+  }
 
   @override
   void dispose() {
